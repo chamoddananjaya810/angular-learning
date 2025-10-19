af4afc60f4d932dc6a46cf6686dd808f8a4a85aa
chamoddananjaya810 <chamoddhananjaya76@gmail.com>
2025-10-19 13:55:03 +0530

    Your meaningful message here

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Products } from './components/products/products';
+import { Login } from './components/login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet
+    ,Products,
+    Login,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('first');
+  protected readonly title = signal('first hello');
 }
--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,13 +1,35 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    standalone:true,
-    selector:'app-login',
-    template:'<h1>login </h1>'
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
 })
+export class Login {
 
 
-export class Login{
+  // 3. HTML input වලට bind කරන්න variables දෙකක් හදමු
+  username: string = '';
+  password: string = '';
 
+  // 4. Button එක click කළාම වැඩ කරන function එක
+  onLogin() {
+    // දැනට, අපි data console එකේ print කරලා බලමු
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+    
+    // (ඇත්ත project එකකදී, මෙතනින් තමයි API call එකක් යවලා userව check කරන්නේ)
 
+    if (this.username === 'admin' && this.password === '123') {
+      console.log('Login Successful!');
+       alert('Login Successful!'); 
+    } else {
+      console.log('Login Failed!');
+       alert('Login Failed! Invalid username or password.');
+    }
+  }
 }
